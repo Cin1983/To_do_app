b76f827b87c3d27522c242ce3bee9e49a1187121
Cin1983! <cinnymaria@gmail.com>
2020-11-12 12:05:11 +0100

    React to do app
    Tutorial

---
+++ to_do_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+ 
+
+function App() {
+  return (
+    <div className="App">
+     <Calendar />
+    </div>
+  );
+}
+
+export default App;
